f923b9a2a1750dc5e8f3f5e54104583202712ff9
PoetLi <poet.li@qq.com>
2020-12-06 20:55:26 +0800

    加上了接口 文件开头有详细注释  我的decode只是得到了string，没有写文件操作

--- (parameter)buildTree.cpp
+++ (parameter)buildTree.cpp
@@ -1,9 +1,19 @@
+/*
+实现buildTree函数
+注意typedef了东西
+调用的参数是三个map变量
+1.stringset是杰哥给的     单词到权重的映射
+2.huffmanCode是单词到01编码的映射   传入encode函数使用
+3.CodeToWord是01编码到单词的映射    传入decode函数使用
+由于要返回两个map变量，用返回值的话稍微麻烦，就按引用传递了
+*/
+
 #include <iostream>
 #include <map>
 #include <queue>
 #include <string>
 #include <vector>
-#define Longest 30
+
 using namespace std;
 
 typedef pair<string, int> node;
@@ -78,66 +88,4 @@ void buildTree(map<string, int> stringset, map<string, string>& huffmanCode,
     getHuffmanCode(root, "", huffmanCode, CodeToWord);
     delete node1;
     delete node2;
-}
-
-void encode(string StrOG, string& StrOf01, map<string, string> huffmanCode) {
-    int TempLength = Longest;
-    //设置一个TempLength变量，初始为最长长度，每次先检测当前字符串头的前TempLength个字符
-    //之和，看看是不是map中对应的一个单词
-    while (StrOG.length() > Longest) {
-        auto iter = huffmanCode.find(StrOG.substr(0, TempLength));
-        if (iter != huffmanCode.end()) {
-            StrOf01 += iter->second;
-            StrOG.erase(StrOG.begin(), StrOG.begin() + TempLength);
-            TempLength = Longest;
-            continue;
-        }
-        TempLength--;
-    }
-}
-
-void decode(string StrOf01, string& StrUnzip, map<string, string> CodeToWord) {
-    int TempLength = 0;
-    while (!StrOf01.empty()) {
-        auto iter = CodeToWord.find(StrOf01.substr(0, TempLength));
-        if (iter != CodeToWord.end()) {
-            StrUnzip += iter->second;
-            StrOf01.erase(StrOf01.begin(), StrOf01.begin() + TempLength);
-            TempLength = 0;
-        }
-        TempLength++;
-    }
-}
-
-// 以下为本地单文件测试代码
-int main() {
-    map<string, int> stringset;       //字符子串与权重的一一映射
-    map<string, string> huffmanCode;  //字符子串与编码的一一映射
-    map<string, string> CodeToWord;   //编码到单词的映射
-    stringset["One  "] = 1;
-    stringset["Two "] = 2;
-    stringset["Forty-Four  "] = 44;
-    stringset["NINE    "] = 9;
-    stringset["99\n    "] = 99;
-    stringset["a thousand minor one|||||"] = 999;
-    buildTree(stringset, huffmanCode, CodeToWord);
-    cout << endl << "----------" << endl;
-    cout << huffmanCode.at("One  ") << endl;
-    cout << huffmanCode.at("Two ") << endl;
-    cout << huffmanCode.at("Forty-Four  ") << endl;
-    cout << huffmanCode.at("NINE    ") << endl;
-    cout << huffmanCode.at("99\n    ") << endl;
-    cout << huffmanCode.at("a thousand minor one|||||") << endl;
-    string StrOG =
-        "One  Two a thousand minor one|||||a thousand minor one|||||a thousand "
-        "minor one|||||a thousand minor one|||||a thousand minor one|||||a "
-        "thousand minor one|||||";
-    // string StrOf01 = "0000000001111111";
-    string StrOf01 = "";
-    string StrUnzip = "";
-    encode(StrOG, StrOf01, huffmanCode);
-    cout << "-----StrOf01-----" << endl << StrOf01 << endl << endl;
-    decode(StrOf01, StrUnzip, CodeToWord);
-    cout << "-----StrUnzip-----" << endl << StrUnzip << endl << endl;
-    return 0;
 }
