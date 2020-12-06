82e13483788a22ee6900e2af94e22492d82b80c2
lhj-git <72364657+lhj-git@users.noreply.github.com>
2020-12-06 15:39:31 +0800

    Update ImprovedHuffman.cpp
    核心部分

--- ImprovedHuffman.cpp
+++ ImprovedHuffman.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #define MAX 100001
+#define MAXWINDOW 3
 using namespace std;
 class matrix
 {
@@ -11,7 +12,7 @@ public:
     double **val;
     matrix(int size = 1)
     {
-        size = size;
+        this->size = size;
         val = (double **)malloc(size * size * sizeof(double));
     }
     ~matrix()
@@ -28,15 +29,17 @@ public:
     int len;            //字符集长度
     double compressionRate;
 
-    double forwardtemp[MAX]; //一阶向前转移概率阈值
-    double backtemp[MAX];    //一阶向后转移概率阈值
+    double forwardtemp1[MAX];    //一阶向前转移概率阈值
+    double backtemp1[MAX];       //一阶向后转移概率阈值
+    double forwardtempn[MAX][5]; //多阶向前转移概率阈值，根据具体的字符串计算得到
+    double backtempn[MAX][5];    //多阶向后转移概率阈值
 
     matrix p1; //一阶向前转移矩阵
     matrix n1; //一阶向后转移矩阵
     matrix p2; //二阶向前转移矩阵
     matrix n2; //二阶向后转移矩阵
     matrix p3; //三阶向前转移矩阵
-    matrix n4; //三阶向后转移矩阵
+    matrix n3; //三阶向后转移矩阵
 
     map<string, int> stringset;      //字符子串与权重的一一映射
     map<string, string> huffmanCode; //字符子串与编码的一一映射
@@ -47,7 +50,9 @@ public:
 
     void statistics(); //数据统计模块，用originalStr计算出一二三阶的向前向后转移阈值与转移矩阵（共6个值，6个矩阵需要计算）
 
-    void calculatetemp(); //计算阈值
+    void calculatetemp1(); //计算阈值
+
+    void calculatetempn(); //计算n阶阈值
 
     void judgeRelation(); //相关性判断模块，由originalStr和转移矩阵构建stringset
 
@@ -70,17 +75,129 @@ public:
     }
 };
 
-void Solution::calculatetemp()
+void Solution::calculatetemp1()
 {
+    for (int i = 0; i < p1.size; i++)
+    {
+        double cnt = 0;
+        double m = MAX;
+        for (int j = 0; j < p1.size; j++)
+        {
+            cnt += p1.val[i][j];
+            m = min(m, p1.val[i][j]);
+            if (cnt >= 0.8)
+            {
+                break;
+            }
+        }
+        forwardtemp1[i] = m;
+    }
+    for (int i = 0; i < p1.size; i++)
+    {
+        double cnt = 0;
+        double m = MAX;
+        for (int j = 0; j < p1.size; j++)
+        {
+            cnt += n1.val[i][j];
+            m = min(m, n1.val[i][j]);
+            if (cnt >= 0.8)
+            {
+                break;
+            }
+        }
+        backtemp1[i] = m;
+    }
+}
 
+void Solution::calculatetempn()
+{
+    for (int i = 0; i < originalStr.size(); i++)
+    {
+        int k = originalStr[i];
+        double ans = forwardtemp1[k];
+        int j = i + 1;
+        for (; j < p1.size && j - i <= 3; j++)
+        {
+            forwardtempn[i][j - i] = ans;
+            ans *= forwardtemp1[j];
+        }
+    }
+}
+
+void Solution::judgeRelation()
+{
+    int posi = 0;
+    int step;
+    int flag = 0;
+    calculatetemp1();
+    calculatetempn();
+    while (posi < len - 3)
+    {
+        for (int i = posi; i < MAXWINDOW; i++)
+        {
+            if (p1.val[i][i + step] >= forwardtemp1[i] && n1.val[i][i + step] >= backtemp1[i])
+            {
+                flag = i;
+                continue;
+            }
+            else
+            {
+                flag = i - 1;
+                break;
+            }
+        }
+
+        //一阶已经完成
+        step = posi + 2;
+        //flag是滑动窗口的右边界
+        if (flag - posi > 1) //零阶直接跳出
+        {
+            int l = flag;
+            for (int j = step; j <= l; j++)
+            {
+                if (j - posi == 2)
+                    if (p2.val[posi][j] >= forwardtempn[posi][j - posi] && n2.val[posi][j] >= backtempn[posi][j - posi])
+                    {
+                        flag = j;
+                        continue;
+                    }
+                    else
+                    {
+                        flag = j - 1;
+                        break;
+                    }
+                if (j - posi == 3)
+                {
+                    if (p3.val[posi][j] >= forwardtempn[posi][j - posi] && n3.val[posi][j] >= backtempn[posi][j - posi])
+                    {
+                        flag = j;
+                        continue;
+                    }
+                    else
+                    {
+                        flag = j - 1;
+                        break;
+                    }
+                }
+            }
+        }
+        string s = originalStr.substr(posi, flag + 1 - posi);
+
+        pair<map<string, int>::iterator, bool> Insert_Pair;
+
+        Insert_Pair = stringset.insert(map<string, int>::value_type(s, 1));
+
+        if (!Insert_Pair.second)
+        {
+            stringset[s]++;
+        }
+        posi += s.size();
+    }
 }
 void Solution::countRate()
 {
-    compressionRate = Strcode.length() / (originalStr.length() * 8);
+    compressionRate = Strcode.size() / (originalStr.size() * 8);
     cout << "The compression rate is " << compressionRate << endl;
-}
-void judgeRelation()
-{
 }
 int main()
 {
